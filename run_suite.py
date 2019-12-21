004abe959215e33b32926a8665d38bacc751652f
zhjytest <email@example.com>
2019-12-21 17:16:25 +0800

    默认的

--- run_suite.py
+++ run_suite.py
@@ -16,7 +16,7 @@ suite.addTest(unittest.makeSuite(TestEmployee))
 
 
 #生成测试报告
-file_path = BASE_DIR+"/report/report_{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+file_path = BASE_DIR+"/report/report.html"
 with open(file_path,'wb') as f:
     runner = HTMLTestRunner(f,title='ihrm测试报告',description='v1.0')
     runner.run(suite)
